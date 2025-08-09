167cb46d2f80b07a7011a2efcf857eeec228affb
ryousuke <ryousuke1039@gmail.com>
2025-08-09 15:42:45 +0900

    chore: Sprting Bootプロジェクトの初期セットアップ

---
+++ src/main/java/dev/kuwa/company_library_system/CompanyLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package dev.kuwa.company_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kuwa/company_library_system/CompanyLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kuwa.company_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
